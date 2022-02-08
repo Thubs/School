5ec562c2e921b5fdfbedbe3186533325e46f2179
Thubs <hughcookpalmeh@gmail.com>
2022-02-08 12:47:11 +1100

    Lesson 1

--- Challenges.py
+++ Challenges.py
@@ -1,7 +1,8 @@
-from tokenize import Intnumber
-
+# Challenge 1
 
 print("hello world")
+
+# Challenge 2
 
 print("the fakers gonna")
 
@@ -13,20 +14,29 @@ print(fake)
 print(fake)
 print(fake)
 
+# Challenge 3
+
 local_people = input('Who are the local people: ')
 
 print("greetings, " + local_people + "!")
+
+# Challenge 4
 
 end_station = input("End station: ")
 
 print("This is the " + end_station + " train survice")
 print("This train will terminate at " + end_station)
 
+# Challenge 5
+
 animal1 = input("What is an animal: ")
 animal2 = input("What is another animal: ")
 
 print("I see a " + animal1 + " " + animal2 + " over there!")
 
+# Challenge 6
+
+# Make sure you pay attention to the int
 numberino = int(input("Input a number to be doubled: "))
 
 doubled = numberino * 2
