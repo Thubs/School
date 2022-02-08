46002841b180184456c403102b4419d2a5496ee3
Thubs <hughcookpalmeh@gmail.com>
2022-02-08 01:44:22 +0000

    Add files via upload

---
+++ Challenges.py
@@ -0,0 +1,34 @@
+from tokenize import Intnumber
+
+
+print("hello world")
+
+print("the fakers gonna")
+
+fake = ('fake')
+
+print(fake)
+print(fake)
+print(fake)
+print(fake)
+print(fake)
+
+local_people = input('Who are the local people: ')
+
+print("greetings, " + local_people + "!")
+
+end_station = input("End station: ")
+
+print("This is the " + end_station + " train survice")
+print("This train will terminate at " + end_station)
+
+animal1 = input("What is an animal: ")
+animal2 = input("What is another animal: ")
+
+print("I see a " + animal1 + " " + animal2 + " over there!")
+
+numberino = int(input("Input a number to be doubled: "))
+
+doubled = numberino * 2
+
+print(doubled)
