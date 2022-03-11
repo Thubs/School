288f2fdeea47fa11c4c34e551555e3ead10d96d5
Thubs <hughcookpalmeh@gmail.com>
2022-03-11 16:55:21 +1100

    Update Codehs_turtle.py

--- Codehs_turtle.py
+++ Codehs_turtle.py
@@ -188,3 +188,30 @@ forward(50)
 left(45)
 color("pink")
 forward(50)
+
+# Circle square triangle
+
+"""
+This program draws three shapes from the same position. Each shape is a 
+different color.
+"""
+
+speed(5)
+
+# Draws a red circle
+color("red")
+begin_fill()
+circle(75)
+end_fill()
+
+# Draws a blue square due to 4 steps
+color("blue")
+begin_fill()
+circle(75,360,4)
+end_fill()
+
+# Draws a yellow triangle due to 3 steps
+color("yellow")
+begin_fill()
+circle(75,360,3)
+end_fill()
